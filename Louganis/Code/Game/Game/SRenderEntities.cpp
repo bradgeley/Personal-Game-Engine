0e2e7974086f4af8771ebc10bafad7971c457e2f
Bradley Christensen <bradjc95@gmail.com>
2025-10-04 00:14:14 -0500

    fix tree render distance calc by using render offset

--- Louganis/Code/Game/Game/SRenderEntities.cpp
+++ Louganis/Code/Game/Game/SRenderEntities.cpp
@@ -111,10 +111,11 @@ void SRenderEntities::Run(SystemContext const& context)
 		InstanceBuffer* ibo = g_renderer->GetInstanceBuffer(iboID);
 
         SpriteInstance instance;
-		float instanceDepth = MathUtils::RangeMap(render.m_pos.y - (render.m_scale * 0.5f), cameraBounds.mins.y - 100.f, cameraBounds.maxs.y + 100.f, 0.05f, 0.95f);
+		float instanceDepth = MathUtils::RangeMap(render.GetRenderPosition().y - (render.m_scale * 0.5f), cameraBounds.mins.y - 100.f, cameraBounds.maxs.y + 100.f, 0.05f, 0.95f);
 		instance.m_position = Vec3(render.GetRenderPosition(), instanceDepth); // todo: z-order
 		instance.m_orientation = render.m_orientation;
 		instance.m_scale = render.m_scale;
+        instance.m_rgba = render.m_tint;
         instance.m_rgba = render.m_tint;
 		instance.m_spriteIndex = anim.m_animInstance.GetCurrentSpriteIndex();
 
