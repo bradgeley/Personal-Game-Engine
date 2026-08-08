7ea730c3817e34bf140113fd92c16db965f8a284
bradgeley <bradjc95@gmail.com>
2026-08-08 13:39:22 -0500

    missing files

--- Daley/Code/Game/Game/SCamera.cpp
+++ Daley/Code/Game/Game/SCamera.cpp
@@ -26,9 +26,6 @@ void SetUICameraBounds(SCCamera& camera, IntVec2 const& renderResolution)
 		viewportDimensions.y = static_cast<int>(renderResolution.x / letterboxAspect);
 	}
 
-	float viewportLeft = (renderResolution.x - viewportDimensions.x) * 0.5f;
-	float viewportTop = (renderResolution.y - viewportDimensions.y) * 0.5f;
-
 	camera.m_uiCamera.SetOrthoBounds2D(AABB2(Vec2::ZeroVector, Vec2((float) viewportDimensions.x, (float) viewportDimensions.y)));
 }
 
@@ -40,16 +37,22 @@ void SCamera::Startup()
     AddReadDependencies<SCWindow>();
     AddWriteDependencies<SCCamera>();
 
-	SCWorld& world = g_ecs->GetSingleton<SCWorld>();
+	SCWorld const& world = g_ecs->GetSingleton<SCWorld>();
+	SCWindow const& window = g_ecs->GetSingleton<SCWindow>();
     SCCamera& camera = g_ecs->GetSingleton<SCCamera>();
-	camera.m_worldCamera.DefineGameSpace(Vec3(0.f, 0.f, -1.f), Vec3(-1.f, 0.f, 0.f), Vec3(0.f, 1.f, 0.f));
-	camera.m_uiCamera.DefineGameSpace(Vec3(0.f, 0.f, -1.f), Vec3(-1.f, 0.f, 0.f), Vec3(0.f, 1.f, 0.f));
+
+	Vec3 gameForward	= Vec3(0.f, 0.f, -1.f);
+	Vec3 gameLeft		= Vec3(-1.f, 0.f, 0.f);
+	Vec3 gameUp			= Vec3(0.f, 1.f, 0.f);
+
+	// Same for UI as game
+	camera.m_worldCamera.DefineGameSpace(gameForward, gameLeft, gameUp);
+	camera.m_uiCamera.DefineGameSpace(gameForward, gameLeft, gameUp);
 
     AABB2 cameraBounds = world.GetVisibleWorldBounds();
     camera.m_worldCamera.SetOrthoBounds2D(cameraBounds);
     camera.m_worldCamera.SetPosition2D(Vec2(0.f, 0.f));
 
-	SCWindow const& window = g_ecs->GetSingleton<SCWindow>();
 	SetUICameraBounds(camera, window.GetWindow()->GetRenderResolution());
 }
 
